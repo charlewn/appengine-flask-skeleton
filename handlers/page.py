23c95b407474f568788a2ace66bdf7deb522a6f8
charlewn <charleswchng@gmail.com>
2017-01-15 21:25:47 +0800

    update

--- handlers/page.py
+++ handlers/page.py
@@ -3,7 +3,7 @@ import logging
 #handlers/repository
 from flask import Blueprint, render_template, request, redirect, make_response
 
-#from models import repo_model
+
 #from models import user_model
 #from models import log_model
 from handlers.base import Handler
